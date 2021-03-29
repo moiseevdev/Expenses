1347058296f5c05e10aaad5f81a77cf3540838d5
Moiseev Andrei <andreimoiseew2009@yandex.ru>
2021-03-29 23:53:44 +0300

    Initial Commit

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Андрей Моисеев on 29.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
